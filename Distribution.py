c45f4b6b38a54562abb557494d48a6a159aa8ef0
Francesco Messina <frmessin@3-I-1.42heilbronn.de>
2026-05-09 04:17:34 +0200

    feat: Distribution WIP

---
+++ Distribution.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import sys
+
+IMGS_PATH='images'
+
+
+def check_path(arg:str) -> None:
+    p = Path(__file__).parent / IMGS_PATH
+
+    if not p.exists():
+        raise FileNotFoundError(f"Path not found: {p}")
+    ls_folder = []
+    for value in p.iterdir():
+        if arg.lower() in value.name.lower():
+            ls_folder.append(value.name)
+
+
+def main() -> None:
+    check_path((sys.argv)[1])
+
+if __name__ == "__main__":
+    main()
